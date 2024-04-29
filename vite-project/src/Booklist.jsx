eb63ceafdc5dc3021948c9e0859263d38d7b53ed
neeraj kr <kumarneeraj18769@gmail.com>
2024-04-29 11:12:10 -0800

    initial commit

---
+++ vite-project/src/Booklist.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const BookList = () =>{
+       return
+       {
+        <div>
+          <Image/>
+          <Title/>
+          <Author/>
+        </div>
+       }
+};
+
+function Title(){
+    return <h1>Book Title</h1>
+}
+const Image = ()=>{
+    
+     return <h3>Author</h3>
+}
+
+const Author = ()=>{
+   return <h3></h3>
+}
